fea4e1b2d933b916bbe662e247cc3b36971dc18f
Lester Cheung <289337+lestercheung@users.noreply.github.com>
2024-03-24 23:42:49 +1100

    Dynamic tap

--- keyboards/annepro2/keymaps/lc-mac/config.h
+++ keyboards/annepro2/keymaps/lc-mac/config.h
@@ -1,3 +1,9 @@
 // my options
+
 // https://github.com/qmk/qmk_firmware/blob/master/docs/feature_grave_esc.md
 // #define GRAVE_ESC_ALT_OVERRIDE
+
+// https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md#permissive-hold
+#define PERMISSIVE_HOLD
+#define HOLD_ON_OTHER_KEY_PRESS
+// #define TAPPING_TERM_PER_KEY
