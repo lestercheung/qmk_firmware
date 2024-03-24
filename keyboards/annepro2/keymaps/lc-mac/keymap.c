ecbf2c5bff643f3073c2717c177cd459aa78acf6
Lester Cheung <289337+lestercheung@users.noreply.github.com>
2024-03-24 23:42:49 +1100

    Dynamic tap

--- keyboards/annepro2/keymaps/lc-mac/keymap.c
+++ keyboards/annepro2/keymaps/lc-mac/keymap.c
@@ -52,11 +52,11 @@ enum anne_pro_layers {
 */
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [BASE] = LAYOUT_60_ansi( /* Base */
-    QK_GRAVE_ESCAPE,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5, KC_6,   KC_7, KC_8,    KC_9,    KC_0,             KC_MINS,   KC_EQL,    KC_BSPC,
-    KC_TAB,           KC_Q,         KC_W,         KC_E,         KC_R,         KC_T, KC_Y,   KC_U, KC_I,    KC_O,    KC_P,             KC_LBRC,   KC_RBRC,   KC_BSLS,
-    LT(FN1, KC_CAPS), LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_H,   KC_J, KC_K,    KC_L,    RCTL_T(KC_SCLN),  KC_QUOT,        KC_ENT,
-    KC_LSFT,          KC_Z,         KC_X,         KC_C,         KC_V,         KC_B, KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH,          RSFT_T(KC_UP),
-    KC_LCTL,          KC_LALT,      KC_LGUI,                                  KC_SPC,                      KC_GRV,  LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
+    KC_GRV,           KC_1,         KC_2,         KC_3,         KC_4,         KC_5, KC_6,         KC_7,         KC_8,          KC_9,  KC_0,             KC_MINS,          KC_EQL,    KC_BSPC,
+    KC_TAB,           KC_Q,         KC_W,         KC_E,         KC_R,         KC_T, KC_Y,         KC_U,         KC_I,          KC_O,  KC_P,             KC_LBRC,          KC_RBRC,   KC_BSLS,
+    LT(FN1, KC_ESC),  LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G, KC_H, RSFT_T(KC_J), RGUI_T(KC_K),  RALT_T(KC_L),  RCTL_T(KC_SCLN),  KC_QUOT,          KC_ENT,
+    KC_LSFT,          KC_Z,         KC_X,         KC_C,         KC_V,         KC_B, KC_N,         KC_M,      KC_COMM,        KC_DOT,  KC_SLSH,          RSFT_T(KC_UP),
+    KC_LCTL,          KC_LALT,      KC_LGUI,                                  KC_SPC,                                        KC_GRV,  LT(FN1, KC_LEFT), LT(FN2, KC_DOWN), RCTL_T(KC_RGHT)
 ),
   /*
   * Layer FN1
@@ -85,9 +85,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,-----------------------------------------------------------------------------------------.
   * |  ~  | BT1 | BT2 | BT3 | BT4 |  F5 |  F6 | F7 | F8 | MOD | TOG | BRI- | BRI+ |    Bksp   |
   * |-----------------------------------------------------------------------------------------+
-  * | Tab    |  q  | UP  |  e  |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
+  * | Tab    |  q  |     |DT_⬆️ |  r  |  t  |  y  |  u  |  i  |  o  | PS | HOME | END |   \    |
   * |-----------------------------------------------------------------------------------------+
-  * | Esc     |LEFT |DOWN |RIGHT|  f  |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
+  * | Esc     |     |     |DT_⬇️ |DT_P |  g  |  h  |  j  |  k  |  l  | PGUP|PGDN |    Enter    |
   * |-----------------------------------------------------------------------------------------+
   * | Shift      |  z  |  x  |  c  |  v  |  b  |  n  |  m  |  ,  |INSRT| DEL |    Shift       |
   * |-----------------------------------------------------------------------------------------+
@@ -97,10 +97,19 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
  [FN2] = LAYOUT_60_ansi( /* FN2 */
     _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______, KC_AP_RGB_MOD, KC_AP_RGB_TOG, KC_AP_RGB_VAD, KC_AP_RGB_VAI, _______,
-    _______, _______,    KC_UP,      _______,    _______,    _______, _______, _______, _______, _______,       KC_PSCR,       KC_HOME,       KC_END,        _______,
-    _______, KC_LEFT,    KC_DOWN,    KC_RGHT,    _______,    _______, _______, _______, _______, _______,       KC_PGUP,       KC_PGDN,       _______,
+    _______, _______,    _______,      DT_UP,    _______,    _______, _______, _______, _______, _______,       KC_PSCR,       KC_HOME,       KC_END,        _______,
+    _______, _______,    _______,    DT_DOWN,    DT_PRNT,    _______, _______, _______, _______, _______,       KC_PGUP,       KC_PGDN,       _______,
     _______,             _______,    _______,    _______,    _______, _______, _______, _______, _______,       KC_INS,        KC_DEL,        _______,
     _______, _______,    _______,                                     _______,                   _______,       _______,       _______,       _______
  ),
 };
 // clang-format on
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case RSFT_T(KC_UP):
+    return 100;
+  default:
+    return TAPPING_TERM;
+  }
+}
