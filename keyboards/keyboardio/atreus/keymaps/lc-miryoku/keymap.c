7c2c05fc21f478f922a4d260a5ae80616feb7caf
Lester Cheung <289337+lestercheung@users.noreply.github.com>
2024-04-05 16:17:36 +1100

    [lc-miryoku] tweaking thumb and pinky keys

--- keyboards/keyboardio/atreus/keymaps/lc-miryoku/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/lc-miryoku/keymap.c
@@ -59,17 +59,17 @@ tap_dance_action_t tap_dance_actions[] = {
 
 
 /*
-| q   | w  | e   | r         | t       |          |       | y     | u       | i | o | p     |
-| a   | s  | d   | f         | g       |          |       | h     | j       | k | l | ;     |
-| z   | x  | c   | v         | b       |          |       | n     | m       | , | . | /     |
-| ESC | S⇑ | GUI | MEDIA/ESC | NAV/SPC | MOUSE/⭾ | SYM/⏎ | NUM/⇦ | FUN/DEL | - | ' | CTRL/⇦ |
+| q   | w  | e   | r       | t       |           |       | y     | u       | i | o | p      |
+| a   | s  | d   | f       | g       |           |       | h     | j       | k | l | ;      |
+| z   | x  | c   | v       | b       |           |       | n     | m       | , | . | /      |
+| ESC | S⇑ | GUI | MEDIA/⭾ | NAV/SPC | MOUSE/ESC | SYM/⏎ | NUM/⇦ | FUN/DEL | - | ' | CTRL/⇦ |
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT( /* Qwerty */
-    KC_Q,        KC_W,        KC_E,             KC_R,              KC_T,                                                KC_Y,            KC_U,          KC_I,        KC_O,        KC_P,
-    CTL_T(KC_A), ALT_T(KC_S), GUI_T(KC_D),      SFT_T(KC_F),       KC_G,                                                KC_H,            SFT_T(KC_J),   GUI_T(KC_K), ALT_T(KC_L), CTL_T(KC_SCLN),
-    KC_Z,        KC_X,        KC_C,             KC_V,              KC_B,            KC_GRV,           KC_BSLS,          KC_N,            KC_M,          KC_COMM,     KC_DOT,      KC_SLSH,
-    KC_ESC,      KC_LSFT,     KC_LGUI,    LT(MEDIA, KC_ESC), LT(NAV, KC_SPC), LT(MOUSE, KC_TAB), LT(SYM, KC_ENT),  LT(NUM, KC_SPC), LT(FUN, KC_BSPC), TD(TD_MINUS_PLUS), KC_QUOT, CTL_T(KC_ENT)),
+    KC_Q,           KC_W,        KC_E,             KC_R,              KC_T,                                                KC_Y,            KC_U,          KC_I,        KC_O,        KC_P,
+    CTL_T(KC_A),    ALT_T(KC_S), GUI_T(KC_D),      SFT_T(KC_F),       KC_G,                                                KC_H,            SFT_T(KC_J),   GUI_T(KC_K), ALT_T(KC_L), CTL_T(KC_SCLN),
+    KC_Z,           KC_X,        KC_C,             KC_V,              KC_B,            KC_GRV,           KC_BSLS,          KC_N,            KC_M,          KC_COMM,     KC_DOT,      KC_SLSH,
+    CTL_T(KC_LEFT), KC_LSFT,     KC_LGUI,    LT(MEDIA, KC_TAB), LT(NAV, KC_SPC), LT(MOUSE, KC_ESC), LT(SYM, KC_ENT),  LT(NUM, KC_SPC), LT(FUN, KC_BSPC), TD(TD_MINUS_PLUS), KC_QUOT, CTL_T(KC_RGHT)),
 
 /*
 | ⛑️  |     |     |     |   |   |   |       | 🔇   |      |       |        |
